1c925cb4c350d79c8d57f14334a3a65ff8d4d88a
bodhisattwa das <bodhisattwadas@users.noreply.github.com>
2021-08-15 23:07:55 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -37,7 +37,7 @@ class CreateUsersTable extends Migration
         });
         DB::table('users')->insert(
             array(
-                ['name'=>'admin','email'=>'admin@kfc.com','verified'=>'yes','password' => Hash::make('irishhngf'),'role' => 'admin'],
+                ['name'=>'admin','email'=>'admin@kfc.com','verified'=>'yes','password' => Hash::make('irishhngf'),'role' => 'admin','phone_number1'=>null],
                 ['name'=>'bodhisattwa','email'=>'das.bodhisattwa@gmail.com','verified'=>'yes','password' => Hash::make('irishhngf'),'role' => 'rider','phone_number1' => '1234567890'],
                 ['name'=>'soumen','email'=>'soumen@gmail.com','verified'=>'yes','password' => Hash::make('soumen2015'),'role' => 'rider','phone_number1' => '5678567889'],
             )
